65d2d6cdb1c2fdd605132923ef1e067db7ddcd75
Paolo <paolocarunia139@gmail.com>
2026-09-04 18:27:32 +0800

    update the CORS

--- server.js
+++ server.js
@@ -158,6 +158,11 @@ app.use(helmet({
 
 const allowedOrigins = [
   'http://localhost:5173',
+  'http://localhost:3000',
+  'http://localhost:5000',
+  'http://127.0.0.1:5173',
+  'http://127.0.0.1:3000',
+  'http://192.168.1.36:5173',
   'https://sta-rosa-nueva-ecija-emergency-response.vercel.app',
   'https://rescuesantarosagov.live',
   'https://www.rescuesantarosagov.live'
